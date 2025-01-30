ce8015cc824e070552be5afc47e92a78a5bd9311
xOAviOx <avishuklacode@gmail.com>
2025-01-30 13:03:02 +0530

    Set up initial commands

--- app.js
+++ app.js
@@ -2,5 +2,15 @@ const minimist = require("minimist");
 
 module.exports = () => {
   const args = minimist(process.argv.slice(2));
-  console.log(args);
+
+  const cmd = args._[0];
+
+  switch (cmd) {
+    case "today":
+      require("./cmds/today")(args);
+      break;
+    default:
+      console.error(`${cmd} is not a valid command`);
+      break;
+  }
 };
